549d960aaeec8007f47c2a1765af53084e09c964
yayanyang <yayanyang@gmail.com>
2016-08-03 11:06:18 +0800

    refactor project tree

--- app/src/main.js
+++ app/src/main.js
@@ -9,7 +9,7 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/bower_components/editor.md/examples/full.html`)
+  win.loadURL(`file://${__dirname}/../static/index.html`)
 
   // Open the DevTools.
   win.webContents.openDevTools()
